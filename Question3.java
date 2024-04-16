5998a702cc145a73542c51aef6b37eba10fe2292
JinXuan03 <148025005+JinXuan03@users.noreply.github.com>
2024-04-16 11:24:33 +0800

    Complete Question 3

--- Question3.java
+++ Question3.java
@@ -6,7 +6,7 @@ public class Question3 {
         Scanner in = new Scanner(System.in);
         
         // Prompt the user to enter an integer
-        System.out.print("Enter an integer: ");
+        //System.out.print("Enter an integer: ");
         
         // Read the integer entered by the user
         int number = in.nextInt();
@@ -15,7 +15,7 @@ public class Question3 {
         int result = number * number;
         
         // Print out the result
-        System.out.println("The result of multiplying " + number + " by itself is: " + result);
+        System.out.println(/*"The result of multiplying " + */number /*+ " by itself is: " +*/ result);
         
         // Close the Scanner object to prevent resource leak
         in.close();
