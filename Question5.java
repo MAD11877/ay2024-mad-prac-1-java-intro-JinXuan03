87423ed74c9d93258e98a19eebcc4edc657aea52
JinXuan03 <148025005+JinXuan03@users.noreply.github.com>
2024-04-16 11:48:44 +0800

    complete Question 5

--- Question5.java
+++ Question5.java
@@ -1,43 +1,40 @@
 import java.util.Scanner;
 
-public class Question5
-{
-  public static void main(String[] args)
-  {
-    /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
-     *    e.g.
-     *     > 5
-     *     > 2
-     *     > 4
-     *     > 1
-     *     > 3
-     *     > 4
-     *     4
-     * 
-     *    e.g.
-     *     > 4
-     *     > 2
-     *     > 2
-     *     > 3
-     *     > 3
-     *     2
-     * Hint: Use a loop to get input. Use another 2 loops to find the mode
-     */
-     
-    Scanner in = new Scanner(System.in);
-    System.out.print("Enter the number of integers: ");
+public class Question5 {
+    public static void main(String[] args) {
+        /**
+         * Prompt the user for number. This input indicates the number of integers the user will be entering next.
+         * Print out the mode (highest occurrence) from the set of integers.
+         * e.g.
+         * > 5
+         * > 2
+         * > 4
+         * > 1
+         * > 3
+         * > 4
+         * 4
+         *
+         * e.g.
+         * > 4
+         * > 2
+         * > 2
+         * > 3
+         * > 3
+         * 2
+         * Hint: Use a loop to get input. Use another 2 loops to find the mode
+         */
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number of integers: ");
         int count = in.nextInt();
 
         int[] numbers = new int[count];
         for (int i = 0; i < count; i++) {
-            System.out.print("Enter integer " + (i + 1) + ": ");
             numbers[i] = in.nextInt();
         }
 
         int mode = findMode(numbers);
-        System.out.println("Mode: " + mode);
+        System.out.println(mode);
     }
 
     public static int findMode(int[] numbers) {
@@ -58,5 +55,6 @@ public class Question5
         }
 
         return mode;
-  }
+    }
 }
+
